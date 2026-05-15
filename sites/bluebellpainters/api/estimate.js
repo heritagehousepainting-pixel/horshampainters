0f0d0ccf40edd9804e885005e4474a85dcc2e91a
heritagehousepainting-pixel <heritagehousepainting@gmail.com>
2026-05-15 00:01:16 -0400

    Apply verified sender to town site APIs

--- sites/bluebellpainters/api/estimate.js
+++ sites/bluebellpainters/api/estimate.js
@@ -1,1 +1,4 @@
+process.env.LEAD_FROM_EMAIL =
+  process.env.LEAD_FROM_EMAIL || "Heritage House Painting <estimates@send.heritagehousepainting.com>";
+
 module.exports = require("../../../api/estimate");
